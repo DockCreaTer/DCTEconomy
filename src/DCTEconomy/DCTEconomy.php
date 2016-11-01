8ada2e4d2e19b4ae28c22424e86fcb67a014920d
TSRlightda <lightda104530@gmail.com>
2016-11-01 23:24:54 +0800

    Create DCTEconomy.php

---
+++ src/DCTEconomy/DCTEconomy.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * DCTEconomy, the economy plugin with for PocketMine-MP
+ * Copyright (C) 2016-2016  DockCreaTer <dockcreater@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+namespace dcteconomy;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\Utils;
+use pocketmine\utils\TextFormat;
+
+class DCTEconomy extends PluginBase implements Listener{
