b51ed023cef6faa8391021d756e5b2bd70a351d8
chadigos <3296764554@qq.com>
2016-11-11 12:01:22 -0600

    Update event.php

--- src/DCTEconomy/event.php
+++ src/DCTEconomy/event.php
@@ -1,1 +1,38 @@
 
+<?php
+
+namespace info;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\entity\Entity;
+use pocketmine\event\player\PlayerJoinEvent;
+
+
+
+class info implements Listener
+{
+
+
+public function __construct(DCTMocomy $main)
+    {
+        $this->m = $main;
+        }
+public function onJoin(PlayerJoinEvent $e){
+    $p=$e->getPlayer();
+    $n=$p->getName();
+  $p->sendmessage("");
+   }
+    }
+  
