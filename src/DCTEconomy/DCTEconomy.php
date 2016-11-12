d6f2bea114b2f78f2c8abc942a4fcc23b2468259
chadigos <3296764554@qq.com>
2016-11-12 01:15:30 -0600

    Add files via upload

--- src/DCTEconomy/DCTEconomy.php
+++ src/DCTEconomy/DCTEconomy.php
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  
-namespace dcteconomy;
+namespace DCTE;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
@@ -27,12 +27,91 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\Utils;
 use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandExecutor;
 
-class DCTEconomy extends PluginBase implements Listener{
+class DCTEconomy extends PluginBase implements CommandExecutor, Listener
+{
+  private static $dct;
   public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getPluginManager()->registerEvents(new event($this), $this);
+       $this->getServer()->getPluginManager()->registerEvents(new Event($this), $this);
         @mkdir($this->getDataFolder());   
-        $this->task= new Config($this->getDataFolder()."PlayerTask.yml", Config::YAML, array());
-        }
-}
+   $this->p=  new Config($this->getDataFolder()."PlayerMoney.yml", Config::YAML, array());
+   $this->config=  new Config($this->getDataFolder()."Config.yml", Config::YAML, ["負債模式"=>"關"]);
+    }
+  
+
+   public function onLoad(){
+		self::$dct= $this;
+}//
+   public static function DCTE(){
+		return static::$dct;
+	}//
+  public function money($p){
+    if($this->in($p) == "ok"){
+  $n=$p->getName();
+  return $this->conf($n);
+  }else{
+  return false;
+  //不是玩家
+  }//-//
+  }//--//
+  public function deduction($p,$deduction){
+ if($this->in($p) == "ok"){
+  $n=$p->getName();
+  $export=$this->conf(strtolower($n)) - $deduction;
+  if($export < 0){
+  if($this->config->get("負債模式")=="開"){
+  $this->setconf($n,$export);
+  $export=$this->conf($n);
+  return $export;
+  }else{
+  $str=0;
+  $this->setconf($n,$str);
+  $export=$this->conf($n);
+  return $export;
+  }//----//
+  }//---//
+  $this->setconf($n,$export);
+  // $this->getLogger()->info("$n $export");
+  return $this->conf($n);
+  
+  }else{
+  return false;
+  //不是玩家
+  }//-//
+  }//--//
+  public function add($p,$add){
+  if($this->in($p) == "ok"){
+  $n=$p->getName();
+  $add=$this->conf($n) + $add;
+  $this->setconf($n,$add);
+  $add=$this->conf($n);
+  
+  }else{
+  return false;
+  //不是玩家
+  }//-//
+  }//--//
+  public function in($p){
+  if($p instanceof Player){
+  $ok="ok";
+  return $ok;
+  }else{
+  $no="no";
+  return $no;
+  }//-//
+  }//--//
+  public function conf($n){
+  $conf=new Config($this->getDataFolder()."PlayerMoney.yml", Config::YAML, array());
+
+  $n=strtolower($n);
+  return $conf->get(strtolower($n));
+  }//-//
+  public function setconf($n,$val){
+  $conf=new Config($this->getDataFolder()."PlayerMoney.yml", Config::YAML, array());
+  $conf->set(strtolower($n),$val);
+  $conf->save();
+  }//-//
+
+  }
